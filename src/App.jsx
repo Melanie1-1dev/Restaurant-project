4125a8f721eba7c52371b5bed6128bd3c775e971
melaniendikubwimana-cloud <melaniendikubwimana@gmail.com>
2026-05-13 14:50:40 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import LandingPage from './Components/LandingPage/LandingPage';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <LandingPage />
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/LandingPage/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './LandingPage.css';
+
+const LandingPage = () => {
+  return (
+    <div>
+
+      
+    </div>
+  )
+}
+
+export default LandingPage
