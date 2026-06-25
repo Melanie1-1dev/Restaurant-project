bac288d8c714e4d2209823620e41a0c6df046aa6
Melanie1-1dev <melaniendikubwimana@gmail.com>
2026-06-25 21:55:49 +0200

    Updated

--- src/Components/Pages/Dashbord/Dashbord.jsx
+++ src/Components/Pages/Dashbord/Dashbord.jsx
@@ -107,19 +107,42 @@ export default function MissMoreDashbord() {
       <aside className="sidebar-navigation">
         <div className="nav-links-group">
           <div className="brand-section">
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M2 3V11C2 13.2 3.8 15 6 15V21H8V15C10.2 15 12 13.2 12 11V3H10V10H9V3H7V10H6V3H4V10H3V3H2ZM16 3C13.8 3 12 4.8 12 7V13H14V21H16V13H18V21H20V13C22.2 13 24 11.2 24 9V3H16ZM16 11V5C17.1 5 18 5.9 18 7V11H16Z" fill="#A16207"/>
             </svg>
             <h1 className="brand-name">MissMore</h1>
           </div>
           
-          <div className="nav-item-btn active"><LayoutGrid size={18} /> Dashboard</div>
-          <Link to="/new-order" style={{ textDecoration: 'none' }}>
-            <button className="nav-item-btn"><ShoppingBag size={18} /> New Order</button>
+          {/* Home Link */}
+          <Link to="/home" style={{ textDecoration: 'none' }}>
+            <div className="nav-item-btn active">
+              <LayoutGrid size={18} /> Home
+            </div>
           </Link>
-       <Link to="/menu-items" style={{ textDecoration: 'none' }}><button className="nav-item-btn"><Utensils size={18} /> Menu</button></Link>
-      <Link to="/an-other-order" style={{textDecoration: 'none'}}><button className="nav-item-btn"><Users size={18} /> Customers</button></Link>
-       <Link to="/profile-dashboard" style={{textDecoration: 'none'}}> <button className="nav-item-btn"><Settings size={18} /> Settings</button> </Link>
+
+          <Link to="/new-order" style={{ textDecoration: 'none' }}>
+            <button className="nav-item-btn">
+              <ShoppingBag size={18} /> New Order
+            </button>
+          </Link>
+
+          <Link to="/menu-items" style={{ textDecoration: 'none' }}>
+            <button className="nav-item-btn">
+              <Utensils size={18} /> Menu
+            </button>
+          </Link>
+
+          <Link to="/an-other-order" style={{ textDecoration: 'none' }}>
+            <button className="nav-item-btn">
+              <Users size={18} /> Customers
+            </button>
+          </Link>
+
+          <Link to="/profile-dashboard" style={{ textDecoration: 'none' }}>
+            <button className="nav-item-btn">
+              <Settings size={18} /> Settings
+            </button>
+          </Link>
         </div>
 
         <div className="kitchen-status-card">
