0851c5279340ccada46f60d65669126befd3f337
Melanie1-1dev <melaniendikubwimana@gmail.com>
2026-08-24 20:28:51 +0200

    Updated

--- src/Components/Pages/NewOrder/NewOrder.jsx
+++ src/Components/Pages/NewOrder/NewOrder.jsx
@@ -107,9 +107,11 @@ export default function NewOrder() {
       <aside className="sidebar-navigation">
         <div className="nav-links-group">
             {/* Logo */}
-                  <div className="home-logo">
-                    <img src={logo} alt="Fast Food Logo" className="logo-img" />
-                  </div>
+            <Link to="/home" style={{ textDecoration: 'none' }}>
+              <div className="home-logo">
+                <img src={logo} alt="Fast Food Logo" className="logo-img" />
+              </div>
+            </Link>
 
           <Link to="/home" style={{ textDecoration: 'none' }}>
             <button className="nav-item-btn">
